7499dade04ad4c35cd447b256489b6f4c833fa05
Alexandre Odoni <aleodoni@gmail.com>
2026-05-15 12:46:05 -0300

    feat(api): Adiciona tabela sincronia

--- internal/database/fdw.go
+++ internal/database/fdw.go
@@ -20,6 +20,7 @@ func RunFDW(cfg *config.Config) error {
 		"fdw/06_projetos.sql",
 		"fdw/07_cuid2.sql",
 		"fdw/08_daily_sync.sql",
+		"fdw/09_tabela_sinc.sql",
 	}
 
 	pool, err := ConnectPGX(cfg)
